330291ad5bd742c6fba8d04f51c3db18947e3943
zxcc0712 <1005691143@qq.com>
2020-08-26 19:58:58 +0800

    改版日期2020.8.16

--- Python基础教程/第三章-使用字符串/字符串.py
+++ Python基础教程/第三章-使用字符串/字符串.py
@@ -3,3 +3,7 @@
 format = 'Hello ,%s. %s enough for ya '
 values = ('world','Hot')
 print(format % values)
+#第二种方式设置字符串
+from string import Template
+tmpl = Template('Hello ,$who! $what enough for ya')
+print(tmpl.substitute(who='Mary',what = 'Dusty'))
