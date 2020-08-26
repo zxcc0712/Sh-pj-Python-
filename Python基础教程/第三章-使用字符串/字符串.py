44afb26afddcda4080e7ef116f127948332a5ed1
zxcc0712 <1005691143@qq.com>
2020-08-26 21:40:45 +0800

    改版日期2020.8.16

--- Python基础教程/第三章-使用字符串/字符串.py
+++ Python基础教程/第三章-使用字符串/字符串.py
@@ -7,3 +7,33 @@ print(format % values)
 from string import Template
 tmpl = Template('Hello ,$who! $what enough for ya')
 print(tmpl.substitute(who='Mary',what = 'Dusty'))
+#完整版字符串格式
+# 1、替换字段名，向format提供要设置其格式的未命名的参数
+'{foo}{}{bar}{}'.format(1,2,bar=6,foo=0)
+# 二进制转换.基本转换
+print('The number is {num:b}'.format(num=42))
+# 设置宽度、精度和千位分隔符
+print('One googol is {:,}'.format(10**100))
+# 字符串左对齐、右对齐、居中分别是<、>和^
+# 最后一个要素是井号（#）
+# 根据指定的宽度打印格式良好的价格列表
+width = int(input('Please enter width:'))
+
+price_width = 10
+item_width = width-price_width
+
+header_fmt = '{{:{}}}{{:>{}}}'.format(item_width,price_width)
+fmt        = '{{:{}}}{{:>{}.2f}}'.format(item_width,price_width)
+print('='*width)
+
+print(header_fmt.format('Item','Price'))
+
+print('-'*width)
+
+print(fmt.format('Apple',0.4))
+print(fmt.format('Pears',0.5))
+print(fmt.format('Cantaloupes',1.92))
+print(fmt.format('Dried Apricots(16 oz.)',8))
+print(fmt.format('Prunes(4 lbs.)',12))
+
+print('='*width)
