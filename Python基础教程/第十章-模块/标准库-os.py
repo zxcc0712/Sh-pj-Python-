58f171afeebe95a6128ed3bf9b83140f439826dc
zxcc0712 <1005691143@qq.com>
2020-09-20 21:45:15 +0800

    改版日期2020.9.20

---
+++ Python基础教程/第十章-模块/标准库-os.py
@@ -0,0 +1,7 @@
+# 模块os让你能够访问多个操作系统服务
+# environ             包含环境变量的映射
+# system(command)     在子shell中执行操作系统命令
+# sep                 路径中使用的分隔符
+# pathsep             分隔不同路径的分隔符
+# linesep             行分隔符（‘\n’,‘\r’,‘\r\n’）
+# urandom(n)          返回n个字节的强加密随机数据
