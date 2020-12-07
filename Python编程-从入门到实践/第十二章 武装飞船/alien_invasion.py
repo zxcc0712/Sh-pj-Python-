6f7c825549e793cb56d870da449fea28c2a0843d
zxcc0712 <1005691143@qq.com>
2020-12-07 20:29:55 +0800

    改版日期2020.12.07

--- Python编程-从入门到实践/第十二章 武装飞船/alien_invasion.py
+++ Python编程-从入门到实践/第十二章 武装飞船/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from settings import Settings
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -8,6 +9,8 @@ def run_game():
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption('Alien Invasion')
 
+    # 设置背景色
+    bg_colour = (230,230,230)
 
     # 开始游戏的主循环
     while True:
@@ -16,6 +19,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环时都重绘屏幕
+        screen.fill(bg_colour)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
