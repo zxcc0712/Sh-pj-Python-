a804006d972a5e45204def7ad99253a5c4216f70
zxcc0712 <1005691143@qq.com>
2020-12-15 21:53:07 +0800

    改版日期2020.12.15

--- Python编程-从入门到实践/第十二章 武装飞船/alien_invasion.py
+++ Python编程-从入门到实践/第十二章 武装飞船/alien_invasion.py
@@ -20,7 +20,7 @@ def run_game():
     aliens = Group()
 
     # 创建外星人群
-    gf.create_fleet(ai_settings,screen,aliens)
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     # 开始游戏的主循环
     while True:
--- Python编程-从入门到实践/第十二章 武装飞船/settings.py
+++ Python编程-从入门到实践/第十二章 武装飞船/settings.py
@@ -5,7 +5,7 @@ class Settings():
         '''初始化游戏的设置'''
         # 屏幕设置
         self.screen_width = 600
-        self.screen_height = 400
+        self.screen_height = 600
         self.bg_colour = (230,230,230)
         # 飞船的设置
         self.ship_speed_factor = 1.2
