2e3a78b98bb910af42d049cd6e00925fb87590ea
zxcc0712 <1005691143@qq.com>
2020-12-23 21:54:18 +0800

    改版日期2020.12.23

--- Python编程-从入门到实践/项目二、数据可视化/scatter_squares.py
+++ Python编程-从入门到实践/项目二、数据可视化/scatter_squares.py
@@ -1,4 +1,19 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4)
-plt.show()
+x_values = list(range(1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,
+            edgecolors='none',s=40)
+
+# 设置图表标题并给坐标轴加上标签
+plt.title('Square Numbers',fontsize =24)
+plt.xlabel('Value',fontsize =14)
+plt.ylabel('Square of Value',fontsize =14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both',which='major',labelsize=14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0,1100,0,1100000])
+
+plt.savefig('squares_plot.png',bbox_inches='tight')
