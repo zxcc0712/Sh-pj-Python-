d78752ad3ae05ed2a8de1074195812ac8371ebf3
zxcc0712 <1005691143@qq.com>
2021-03-02 22:54:05 +0800

    改版日期2021.03.02

--- Python编程-从入门到实践/Web应用程序/learning_log/learning_logs/models.py
+++ Python编程-从入门到实践/Web应用程序/learning_log/learning_logs/models.py
@@ -9,3 +9,16 @@ class Topic (models.Model):
     def __str__(self):
         '''返回模型的字符串表示'''
         return self.text
+
+class Entry(models.Model):
+    '''学习的有关某一个主题的具体知识'''
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        '''返回模型的字符串表示'''
+        return self.text[:50]+'...'
