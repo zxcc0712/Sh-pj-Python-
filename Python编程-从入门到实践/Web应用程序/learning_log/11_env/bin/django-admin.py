a6281c7b18752e9d004c75701763f0282f2f57c7
zxcc0712 <1005691143@qq.com>
2021-03-04 18:43:12 +0800

    改版日期2021.03.04

--- Python编程-从入门到实践/Web应用程序/learning_log/11_env/bin/django-admin.py
+++ Python编程-从入门到实践/Web应用程序/learning_log/11_env/bin/django-admin.py
@@ -1,21 +1,5 @@
 #!/Users/shihe/Downloads/Sh-pj-Python-/Python编程-从入门到实践/Web应用程序/learning_log/11_env/bin/python3
-# When the django-admin.py deprecation ends, remove this script.
-import warnings
-
 from django.core import management
 
-try:
-    from django.utils.deprecation import RemovedInDjango40Warning
-except ImportError:
-    raise ImportError(
-        'django-admin.py was deprecated in Django 3.1 and removed in Django '
-        '4.0. Please manually remove this script from your virtual environment '
-        'and use django-admin instead.'
-    )
-
 if __name__ == "__main__":
-    warnings.warn(
-        'django-admin.py is deprecated in favor of django-admin.',
-        RemovedInDjango40Warning,
-    )
     management.execute_from_command_line()
--- Python编程-从入门到实践/Web应用程序/learning_log/learning_logs/views.py
+++ Python编程-从入门到实践/Web应用程序/learning_log/learning_logs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     '''学习笔记的主页'''
     return render(request,'learning_logs/index.html')
+
+def topic(request,topic_id):
+    '''显示所有的主题 '''
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic,'entries':entries}
+    return render(request,'learning_logs/topic.html',context)
