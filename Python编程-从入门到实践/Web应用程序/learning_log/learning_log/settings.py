6f7b5f938c1f4fd3af8281e209050231921cc4de
zxcc0712 <1005691143@qq.com>
2021-03-10 23:17:17 +0800

    改版日期2021.03.10

--- Python编程-从入门到实践/Web应用程序/learning_log/learning_log/settings.py
+++ Python编程-从入门到实践/Web应用程序/learning_log/learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 我的应用程序
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_logs/templates/learning_logs')],
+        #os.path.join(BASE_DIR, 'templates')没了这句，
+        # 会显示django.template.exceptions.TemplateDoesNotExist: index.html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Python编程-从入门到实践/Web应用程序/learning_log/learning_log/urls.py
+++ Python编程-从入门到实践/Web应用程序/learning_log/learning_log/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import include,url
 
 urlpatterns = [
     url(r'^admin/',include(admin.site.urls)),
+    url(r'^users/',include('users.urls',namespace='users')),
     url(r'',include('learning_logs.urls',namespace='learning_logs')),
 ]
